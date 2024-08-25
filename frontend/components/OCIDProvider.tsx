09a7095b15b32e2aa5d6a9e4596fd49b2d55d986
Asharib Ali <asharibarain4@gmail.com>
2024-08-25 14:29:10 +0500

    Update OCIDProvider.tsx

--- frontend/components/OCIDProvider.tsx
+++ frontend/components/OCIDProvider.tsx
@@ -8,7 +8,7 @@ interface OCIDProviderProps {
 }
 
 const opts = {
-  redirectUri: "http://localhost:3000/redirect",
+  redirectUri: "https://demo.ced.asharib.xyz/redirect",
 };
 
 const OCIDProvider: FC<OCIDProviderProps> = ({ children }) => (
