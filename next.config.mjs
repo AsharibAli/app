4864cd0a0c117b3edf2d7df998285668c471908c
Asharib <asharibarain4@gmail.com>
2024-09-28 11:25:42 +0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'github.com',
+          pathname: '/**',
+        },
+      ],
+    },
+
+};
 
 export default nextConfig;
