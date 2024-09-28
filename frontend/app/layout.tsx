d39da2005bf97622a0fe7d5fa5b0fcd202e01c5b
Asharib <asharibarain4@gmail.com>
2024-09-28 10:41:30 +0500

    update

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import OCIDProvider from "../components/OCIDProvider";
-
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -20,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <OCIDProvider>{children}</OCIDProvider>
+        <Analytics />
       </body>
     </html>
   );
